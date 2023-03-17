f9431e4aafc0c8aa433a0fed68f957fe44590458
Paul Franklin <paul219@revature.net>
2023-03-17 10:34:16 -0400

    Implemented

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(convert(5));
+        System.out.println(convert(3));
+        System.out.println(convert(2));
+    };
+
+    public static int convert(int minutes) {
+        return minutes * 60;
+    }
+}
